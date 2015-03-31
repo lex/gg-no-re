0e1e64cdbc3eea1007fe74118b789f41c65e721c
atte <atke@cs.helsinki.fi>
2015-03-31 14:47:29 +0300

    add app routes

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'ok'
+    return 'sick welcome screen'
+
+@app.route('/list/<page_number>')
+def paged(page_number):
+    return give_page(page_number)
+
+def give_page(number):
+    return number;
 
 if __name__ == '__main__':
     app.debug = True
