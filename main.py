7368d747e0294388ce2a40a010598da14429b1f3
atte <atke@cs.helsinki.fi>
2015-04-01 21:37:48 +0300

    add placeholder book and a template for it

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+
+from flask import Flask, flash, request, render_template
+from json import JSONEncoder
 app = Flask(__name__)
+
+class Book:
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+
+class Enc(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
 @app.route('/')
 def main():
-    return 'sick welcome screen'
+    return 'sick welcome screen with zooming stuff'
 
 @app.route('/<page_number>')
 def paged(page_number):
     return give_page(page_number)
 
 def give_page(number):
-    return number;
+    b = Book('Juuh vol. 5', 'Juuh Elikkäs')
+    return render_template('list.html', json = Enc().encode(b));
 
 if __name__ == '__main__':
     app.debug = True
