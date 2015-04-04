fdff4bac93322635dfd0256d77aec8db9be4f5ab
lex <lex@oispa.beer>
2015-04-04 16:55:59 +0300

    adding and listing books working

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask, flash, request, render_template
+from flask import Flask, request, render_template
 from json import JSONEncoder
+from ming import create_datastore, Session, collection, Field, Document, schema
+bind = create_datastore('ggnore')
+session = Session(bind)
 app = Flask(__name__)
 
+BookModel = collection(
+        'reference_book', session,
+        Field('_id', schema.ObjectId),
+        Field('title', str),
+        Field('author', str),
+        Field('pages', str),
+        Field('year', str),
+        Field('publisher', str))
+
 class Book:
-    def __init__(self, name, author):
-        self.name = name
+    def __init__(self, title, author, pages, year, publisher):
+        self.title = title
         self.author = author
+        self.pages = pages
+        self.year = year
+        self.publisher = publisher
 
 class Enc(JSONEncoder):
     def default(self, o):
@@ -15,17 +29,29 @@ class Enc(JSONEncoder):
 
 @app.route('/')
 def main():
-    return 'sick welcome screen with zooming stuff'
+    return render_template('index.html', content = list_books_as_json())
 
-@app.route('/<page_number>')
-def paged(page_number):
-    return give_page(page_number)
+@app.route('/add_book', methods=['GET', 'POST'])
+def book_adding():
+    if request.method == 'GET':
+        return render_template('add_book.html')
+    else:
+        r = request.form
+        print r['title']
+        add_book(r['title'], r['author'], r['pages'], r['year'], r['publisher'])
+        return main()
 
-def give_page(number):
-    books = []
-    for i in range(0, int(number)):
-        books.append(Book('Juuh vol. {}'.format(i), 'Juuh Elikkäs'))
-    return render_template('list.html', json = Enc().encode(books));
+def list_books_as_json():
+    books = BookModel.m.find().all()
+    book_list = []
+    for b in books:
+        book_list.append(Book(b.title, b.author, b.pages, b.year, b.publisher))
+
+    return Enc().encode(book_list)
+
+def add_book(title, author, pages, year, publisher):
+    b = BookModel(dict(title = title, author = author, pages = pages, year = year, publisher = publisher))
+    b.m.save()
 
 if __name__ == '__main__':
     app.debug = True
