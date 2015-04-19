184f62c9e56c15de914e44300a7aa08ea4d3ed60
Kim Bäckström <kim.backstrom@helsinki.fi>
2015-04-19 20:52:50 +0300

    Change install.sh to work with jenkins

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://bootstrap.pypa.io/get-pip.py -O - | python - --user
-/usr/local/bin/pip install virtualenv
+python -m pip install virtualenv
 virtualenv venv
 . venv/bin/activate
-usr/local/bin/pip install flask
-usr/local/bin/pip install ming
+python -m pip install flask
+python -m pip install ming
