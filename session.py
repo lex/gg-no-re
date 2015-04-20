f247fe3dd0fd2975f7de65498272041b5af34588
atte <atke@cs.helsinki.fi>
2015-04-20 18:47:24 +0300

    remove unnecessary duplicated session

--- session.py
+++ session.py
@@ -1,10 +1,11 @@
 from ming import create_datastore, Session
 import os
 
+# use the inmemory database if we're running tests
 if os.getenv('TEST') == 'y':
     bind = create_datastore('mim://localhost:27017', database='test')
-    session = Session(bind)
 else:
     bind = create_datastore('ggnore')
-    session = Session(bind)
+
+session = Session(bind)
 
